3f5044b7b2268e8544fd87ef1f69eb3cc6c6a578
SamuelWebsterIs <84517284+Samuel-Webster-Is-Da-Best@users.noreply.github.com>
2021-06-16 16:52:16 +0000

    s

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// recieve input
 document.getElementById('btn').addEventListener('click', calc)
 
 function calc () {
+  // recieve input value and save as var
   const diam = parseInt(document.getElementById('inp').value)
+  // calculations
   const circ = 2 * Math.PI * (diam / 2)
   const area = Math.PI * (diam / 2) * (diam / 2)
+  // return values
   document.getElementById('ans1').innerHTML = area
   document.getElementById('ans2').innerHTML = circ
 }
